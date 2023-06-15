d6b93528d46766d261d178aef217e6b3552197b8
Hector <hectordm65@gmail.com>
2023-06-15 09:36:33 -0700

    Create blueprint page 📘

---
+++ src/app/blueprint/page.tsx
@@ -0,0 +1,9 @@
+// todo: create beautiful page linking all resources used
+/*
+Tailwind css
+Next.js
+GitHub pages
+ */
+export default function Blueprint() {
+  return <>how this page was made</>;
+}
