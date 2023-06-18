26a5afaa97a989b206ef6b83c305aa6f25c543ac
Hector <hectordm65@gmail.com>
2023-06-18 00:18:06 -0700

    Add support for viewbox 🎁

--- src/app/travel/country.tsx
+++ src/app/travel/country.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { Map } from '../components/map';
+import { Map } from './components/map';
 import { UpdatedMapEntry } from '../types/mapDataTypes';
-import { MapLegend } from '../components/mapLegend';
+import { MapLegend } from './components/mapLegend';
 
 export function Country({
   data,
   name,
-  capitalLocation,
+  capitalLocation = <></>,
+  viewBox,
 }: {
   data: UpdatedMapEntry[];
   name: string;
-  capitalLocation: JSX.Element;
+  capitalLocation?: JSX.Element;
+  viewBox: string;
 }) {
   const [updatedCountryData, setUpdatedCountryData] = useState<UpdatedMapEntry[]>(data);
 
@@ -22,11 +24,16 @@ export function Country({
   };
 
   return (
-    <>
+    <div className="mt-6 space-y-4 flex flex-col items-center">
       <div className="mt-6 text-3xl">{name}</div>
-      <Map countryData={updatedCountryData} onHover={updateStateData} capitalLocation={capitalLocation} />
+      <Map
+        countryData={updatedCountryData}
+        onHover={updateStateData}
+        capitalLocation={capitalLocation}
+        viewBox={viewBox}
+      />
       <p>States visited: {data.reduce((acc, cur) => (cur.visited ? ++acc : acc), 0)}</p>
       <MapLegend countryData={updatedCountryData} onHover={updateStateData} />
-    </>
+    </div>
   );
 }
--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { usaData, mexicoData, australiaData } from './data/mapData';
+import { usaData, mexicoData, australiaData, colombiaData, canadaData } from './data/mapData';
 import { Country } from './country';
 
 export default function Travel() {
@@ -9,15 +9,16 @@ export default function Travel() {
       <Country
         data={usaData}
         name="USA"
+        viewBox="5 0 930 600"
         capitalLocation={
           <circle id="DC" fill="#037ffc" stroke="#000000" strokeWidth="1.5" cx="801.3" cy="251.8" r="5" />
         }
       />
       {/* todo: figure out how to get capital */}
-      {/* todo: center content */}
-      <Country data={mexicoData} name="Mexico" capitalLocation={<></>} />
-      {/* todo: fix styling */}
-      <Country data={australiaData} name="Australia" capitalLocation={<></>} />
+      <Country data={mexicoData} name="Mexico" viewBox="0 0 1000 640" />
+      <Country data={australiaData} name="Australia" viewBox="-150 -5 800 430" />
+      <Country data={canadaData} name="Canada" viewBox="0 30 780 480" />
+      <Country data={colombiaData} name="Colombia" viewBox="-400 100 2000 1050" />
     </main>
   );
 }
