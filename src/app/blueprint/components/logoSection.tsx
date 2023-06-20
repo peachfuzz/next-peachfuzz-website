19836066ae9b82cea7afc62f31c24a1883f65856
Hector <hectordm65@gmail.com>
2023-06-19 22:02:42 -0700

    Change color for blueprint page 🟦

--- src/app/blueprint/components/logoSection.tsx
+++ src/app/blueprint/components/logoSection.tsx
@@ -2,7 +2,7 @@ import { IconText } from '@/app/components/icons/iconText';
 
 export function LogoSection({ title, logo, message }: { title: string; logo: JSX.Element; message: string }) {
   return (
-    <div className="not-prose relative bg-slate-50 rounded-xl dark:bg-slate-800/25">
+    <div className="not-prose relative bg-slate-50 rounded-xl dark:bg-gray-800">
       <div className="flex sm:block sm:overflow-visible">
         <div className="flex items-center justify-around gap-12 sm:gap-4 font-mono font-bold p-4">
           <IconText sectionOne={logo} sectionTwo={<p>{message}</p>} />
