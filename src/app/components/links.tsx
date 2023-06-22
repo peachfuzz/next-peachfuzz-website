6a9f51879f904b4405d0e59e462434875999682f
Hector <hectordm65@gmail.com>
2023-06-21 19:51:45 -0700

    Update correct link component 👨🏽‍🔧

--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -2,7 +2,11 @@ import Link from 'next/link';
 
 export function ExternalLink({ href, label }: { href: string; label: string }) {
   return (
-    <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={href} target="_blank">
+    <a
+      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+      href={`https://${href}`}
+      target="_blank"
+    >
       {label}
     </a>
   );
@@ -10,7 +14,7 @@ export function ExternalLink({ href, label }: { href: string; label: string }) {
 
 export function InternalLink({ href, label }: { href: string; label: string }) {
   return (
-    <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={`https//:${href}`}>
+    <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={href}>
       {label}
     </Link>
   );
