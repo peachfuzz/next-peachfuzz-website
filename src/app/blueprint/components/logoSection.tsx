eec45572fbf4bab10e045445b906b8cab5b7e755
Hector <hectordm65@gmail.com>
2023-06-21 20:07:28 -0700

    Update LogoSection 👌

--- src/app/blueprint/components/logoSection.tsx
+++ src/app/blueprint/components/logoSection.tsx
@@ -1,11 +1,27 @@
 import { IconText } from '@/app/components/icons/iconText';
+import { ExternalLink } from '@/app/components/links';
 
-export function LogoSection({ logo, message }: { logo: JSX.Element; message: JSX.Element }) {
+export function LogoSection({
+  href,
+  linkLabel,
+  logo,
+  messageStart,
+}: {
+  href: string;
+  linkLabel: string;
+  logo: JSX.Element;
+  messageStart: string;
+}) {
+  const message = (
+    <p>
+      {messageStart} <ExternalLink href={href} label={linkLabel} />
+    </p>
+  );
   return (
     <div className="not-prose relative bg-slate-50 rounded-xl dark:bg-gray-800">
       <div className="flex sm:block sm:overflow-visible">
         <div className="flex items-center justify-around gap-12 sm:gap-4 font-mono font-bold p-4">
-          <IconText sectionOne={logo} sectionTwo={<p>{message}</p>} />
+          <IconText sectionOne={logo} sectionTwo={message} />
         </div>
       </div>
     </div>
--- src/app/blueprint/page.tsx
+++ src/app/blueprint/page.tsx
@@ -1,4 +1,3 @@
-// todo: create beautiful page linking all resources used
 import {
   GitIcon,
   GitHubIcon,
@@ -7,23 +6,44 @@ import {
   TypeScriptIcon,
   VercelIcon,
 } from '../components/icons/blueprintIcons';
+import { HeroIconsIcon, WrenchScrewdriverIcon } from '../components/icons/heroIcons';
 import { LogoSection } from './components/logoSection';
 
 /*
-Hero Icons
-Wikipedia icons
+Wikipedia map
 https://paintmaps.com/
  */
 export default function Blueprint() {
   return (
     <main className="mb-auto flex flex-col items-center p-6 md:p-24">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <LogoSection logo={<NextJsIcon />} message="Built with Next.js" />
-        <LogoSection logo={<TailwindCssIcon />} message="Styled with Tailwind CSS" />
-        <LogoSection logo={<VercelIcon />} message="Hosted on Vercel" />
-        <LogoSection logo={<TypeScriptIcon />} message="Typed with TypeScript" />
-        <LogoSection logo={<GitHubIcon />} message="Code stored on GitHub" />
-        <LogoSection logo={<GitIcon />} message="Code managed with Git" />
+      <div className="flex space-x-2">
+        <h1 className="text-5xl">Blueprint</h1>
+        <WrenchScrewdriverIcon width="55px" />
+      </div>
+      <div className="pt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <LogoSection logo={<NextJsIcon />} messageStart="Built with" href="nextjs.org" linkLabel="Next.js" />
+        <LogoSection
+          logo={<TailwindCssIcon />}
+          messageStart="Styled with"
+          href="tailwindcss.com"
+          linkLabel="Tailwind CSS"
+        />
+        <LogoSection logo={<VercelIcon />} messageStart="Hosted on" href="vercel.com" linkLabel="Vercel" />
+        <LogoSection
+          logo={<TypeScriptIcon />}
+          messageStart="Typed with"
+          href="www.typescriptlang.org"
+          linkLabel="TypeScript"
+        />
+        <LogoSection logo={<GitHubIcon />} messageStart="Code stored on" href="github.com" linkLabel="GitHub" />
+
+        <LogoSection logo={<GitIcon />} messageStart="Code managed with" href="git-scm.com" linkLabel="Git" />
+        <LogoSection
+          logo={<HeroIconsIcon width="225px" />}
+          messageStart="Non-brand icons borrowed from"
+          href="heroicons.dev"
+          linkLabel="Hero Icons"
+        />
       </div>
     </main>
   );
