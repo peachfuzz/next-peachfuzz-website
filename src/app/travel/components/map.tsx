8bf7ef8ae865fbf80c1e9b95bd793199ccf76cb4
Hector <hectordm65@gmail.com>
2025-12-10 21:16:24 -0600

    Temporarily revert parent styling 🪮

--- src/app/travel/components/map.tsx
+++ src/app/travel/components/map.tsx
@@ -13,7 +13,8 @@ export function Map({
   viewBox: string;
 }) {
   return (
-    <div className="flex max-h-[25rem] min-w-full items-center justify-center">
+    <div className="w-2/3">
+      {/* <div className="flex max-h-96 min-w-full items-center justify-center"> */}
       <svg height="100%" viewBox={viewBox}>
         <g>
           {countryData.map(({ abbreviation, drawing, hovered, visited }, index) => (
